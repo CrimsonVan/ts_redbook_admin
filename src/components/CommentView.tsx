042d21c7e10de2b63b6de9b36a0e27e8492bc545
crimsonvan <2329016850@qq.com>
2024-12-23 17:58:40 +0800

    完成评论的详情展示

--- src/components/CommentView.tsx
+++ src/components/CommentView.tsx
@@ -1,6 +1,7 @@
 import { Modal } from 'antd'
 import { useState } from 'react'
 import React, { useImperativeHandle } from 'react'
+import './CommentView.scss'
 const CommentView = (props: any, ref: any) => {
   console.log('打印父传子的函数参数', props)
   useImperativeHandle(ref, () => ({
@@ -9,11 +10,11 @@ const CommentView = (props: any, ref: any) => {
   //帖子详情弹窗是否显示
   const [isModalOpen, setIsModalOpen] = useState(false)
   //当前帖子信息
-  //   const [curComment, setCurComment] = useState<any>()
+  const [curComment, setCurComment] = useState<any>()
   //显示弹窗操作
   const showModal = (comment: any) => {
     console.log('打印view的comment', comment)
-    // setCurComment(comment)
+    setCurComment(comment)
     setIsModalOpen(true)
   }
   //修改贴文状态为通过
@@ -24,7 +25,48 @@ const CommentView = (props: any, ref: any) => {
   const handleCancel = () => {
     setIsModalOpen(false)
   }
-
+  // avatar
+  // :
+  // "http://47.109.186.26:3007/uploads/b903b8599bbee78786cb4cee8f7090ae"
+  // child_length
+  // :
+  // 0
+  // comment_id
+  // :
+  // 184
+  // content
+  // :
+  // "牛"
+  // key
+  // :
+  // 184
+  // nick_name
+  // :
+  // "向南墙"
+  // parent_comment_id
+  // :
+  // 0
+  // post_id
+  // :
+  // 120
+  // post_nickname
+  // :
+  // "开心鸡肉卷"
+  // preContent
+  // :
+  // "牛"
+  // reply_comment_id
+  // :
+  // 0
+  // reply_nickname
+  // :
+  // "开心鸡肉卷"
+  // reply_username
+  // :
+  // "0"
+  // username
+  // :
+  // "13114209344"
   return (
     <Modal
       width={500}
@@ -36,7 +78,14 @@ const CommentView = (props: any, ref: any) => {
       onCancel={handleCancel}
       maskClosable={true}
     >
-      <div className="comment">{/* <img className='comment_avatar' src="" alt="" /> */}</div>
+      <div className="comment">
+        <img src={curComment?.avatar} alt="" className="comment_avatar" />
+        <div className="comment_main">
+          <div className="comment_name">{curComment?.nick_name}</div>
+          <div className="comment_content">{curComment?.content}</div>
+          <div className="comment_bottom">昨天 10:12 北京</div>
+        </div>
+      </div>
     </Modal>
   )
 }
