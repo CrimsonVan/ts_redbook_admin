a7653a2eebe53e81cfb17302632ffa98c1016a76
niexy2 <niexy2@finchina.com>
2025-02-21 17:41:09 +0800

    引入useMemo和useSize

--- src/pages/Movie/movie.tsx
+++ src/pages/Movie/movie.tsx
@@ -2,18 +2,39 @@ import Styles from './movie.module.less'
 import styled from 'styled-components'
 import cn from 'classnames'
 import { useData } from './useData'
+import { useSize } from 'ahooks'
+import { useRef } from 'react'
 function Movie() {
+  //useRef
+  const domRef = useRef<any>(null)
   //自定义hooks
-  useData()
+  const { useMemoArr, setStatus, movie, setMovie } = useData()
+  //熟悉useSize
+  const size = useSize(domRef)
+  console.log('size', size)
+
   return (
     <>
       <div className={Styles['movie']}>
         <div className={cn(Styles['movie-item'], { [Styles.active]: true })}></div>
       </div>
-      <div className={Styles.movie2}></div>
+      <div className={Styles.movie2} ref={domRef}></div>
       <StyleComp>
-        <div className="sc-item">test</div>
+        <div className="sc-item">111</div>
       </StyleComp>
+      <div></div>
+      <div>
+        <button onClick={() => setMovie('蝙蝠侠')}>切换</button>
+      </div>
+      <p>{movie}</p>
+      <div>
+        <button onClick={() => setStatus('否')}>筛选</button>
+      </div>
+      {useMemoArr.map((item, index) => (
+        <div key={index} className={Styles['list-item']}>
+          {item}
+        </div>
+      ))}
     </>
   )
 }
@@ -23,6 +44,7 @@ const StyleComp = styled.div`
   background-color: green;
   position: sticky;
   display: flex;
+  overflow: hidden;
   .sc-item {
     width: 40px;
     background-color: yellowgreen;
